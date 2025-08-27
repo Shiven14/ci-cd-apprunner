654531036ff6c176f8050042df520b54edfae784
Shiven H Patel <64271093+Shiven14@users.noreply.github.com>
2025-08-27 01:01:09 +0000

    Fix base path for GitHub Pages deployment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: repo name here
-  base: '/ci-cd-apprunner/',
+  base: '/url-shortener/',
 })
